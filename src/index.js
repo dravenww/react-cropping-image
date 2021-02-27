18d728eb31628913693cb40130def791ad424e65
dravenwu <dravenwu@tencent.com>
2021-02-27 12:52:03 +0800

    base64

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
 import ScreenShot from "./lib";
@@ -7,16 +7,30 @@ import image from './demo.jpeg'
 
 import './index.sass'
 
-const App = () => (
+const App = () => {
+    const cacheRef = React.createRef();
+    const [src, setSrc] = useState(false);
+    const getImage = () => {
+        let base = cacheRef.current.getImage();
+        setSrc(base)
+    }
+    const onEnd = (base) => {
+        setSrc(base)
+    }
+    return (
     <div style={{ width: 640, margin: "15px auto" }}>
         <h1>Hello React</h1>
+        <button onClick={getImage}>getImage</button>
         <section className="img-container">
             <ScreenShot
                 img={image}
+                onMoveEnd={onEnd}
+                ref={cacheRef}
             >
             </ScreenShot>
         </section>
+        {src && <img src={src}/>}
     </div>
-);
+)};
 
 ReactDOM.render(<App/>, document.getElementById("root"));
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useCallback} from "react"
+import {useRef, useState, useEffect, useCallback, forwardRef, useImperativeHandle} from "react"
 import Draggable from 'react-draggable';
 import './index.sass'
 /**
@@ -7,42 +7,75 @@ import './index.sass'
  * @returns {JSX.Element}
  * @constructor
  */
-export default function ScreenShot(props) {
-    console.log(props)
-    const [position, setPosition] = useState({
+const ScreenShot = forwardRef(function (props, ref) {
+    const image = useRef(null);
+    // target元素的宽高
+    const [entity, setEntity] = useState({
         width: 200,
-        height: 200
+        height: 200,
     });
+    // target元素的位置
+    const [position, setPosition] = useState({
+        top: 0,
+        left: 0
+    });
+    // 生成base64类型的图片
+    const generateBase = () => {
+        let canvas = document.createElement('canvas');
+        canvas.setAttribute("width", entity.width)
+        canvas.setAttribute("height", entity.height);
+        let ctx = canvas.getContext('2d')
+        ctx.drawImage(image.current, position.left, position.top, entity.width, entity.height,0, 0, entity.width, entity.height)
+        return canvas.toDataURL('image/png');
+    }
+
+    // 向外暴露方法
+    useImperativeHandle(ref, () => ({
+        getImage() {
+            return generateBase();
+        }
+    }))
+    // 初始化时获取图片，设置宽高和位置
     useEffect(() => {
         let img = new Image();
         img.src = props.img;
         img.onload = () => {
-            setPosition({
-                width: img.height / 2,
+            setEntity({
+                width: img.width / 2,
                 height: img.height / 2,
             })
+            setPosition({
+                top: img.height / 4,
+                left: img.width / 4,
+            })
+            image.current = img;
         }
     }, [props.img])
-    const onStart = useCallback((event, data) => {
-        console.log('start')
-        console.log(data)
-    })
+
+    // 移动结束的回调
     const onStop = useCallback((event, data) => {
-        console.log(data)
+        const width = entity.width / 2;
+        const height = entity.height / 2;
+        const left = data.lastX + width
+        const top = data.lastY + height
+        setPosition({
+            left: left,
+            top: top
+        })
+        setTimeout(() => {
+            props.onMoveEnd && props.onMoveEnd(generateBase());
+        }, 10)
     })
-    const dragHandlers = {
-        onStart,
-        onStop
-    }
+
     const style = {
-        width: position.width + 'px',
-        height: position.height + 'px',
+        width: entity.width + 'px',
+        height: entity.height + 'px',
     }
     return (
         <div className="screen-shot-container">
             <img src={props.img}/>
             <div className="crop-container">
-                <Draggable bounds="parent" {...dragHandlers}>
+                <Draggable bounds="parent" onStop={onStop}>
                     <div className="target" style={style}>
                         {props.children}
                     </div>
@@ -50,4 +83,6 @@ export default function ScreenShot(props) {
             </div>
         </div>
     )
-}
+})
+
+export default ScreenShot;
